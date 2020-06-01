cfbcba75115dafe3d458322a75a3a57272c46df2
CJ Ricciardi <ricciardistg@gmail.com>
2020-06-01 19:29:03 -0400

    Homework completed through 10_functions, and 11_args was started.

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -34,11 +34,14 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # Write a function `print_tuple` that prints all the values in a tuple
 
-# YOUR CODE HERE
+def print_tuple(tuple):
+    """This function will print the each value in a tuple on a seperate line."""
+    for t in tuple:
+        print(t)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work? The ','.
 print_tuple(u)
